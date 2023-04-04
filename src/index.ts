d8f9855822de4fb9f2cb6c3b12c4ad62e07fead1
Yusuke Wada <yusuke@kamawada.com>
2023-04-04 15:26:33 +0900

    refactored

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,50 @@
-import { Env, Hono, HonoRequest } from "hono";
-import { Client } from "twitter-api-sdk";
-import { Configuration, OpenAIApi } from "openai";
-import fetchAdapter from "@vespaiach/axios-fetch-adapter";
-import * as querystring from "querystring";
-import url from "url";
-import path from "path";
+import { Hono } from 'hono'
+import { Client } from 'twitter-api-sdk'
+import { Configuration, OpenAIApi } from 'openai'
+import fetchAdapter from '@vespaiach/axios-fetch-adapter'
 
-const app = new Hono();
+type Bindings = {
+  TWITTER_BEARER_TOKEN: string
+  OPENAI_ORGANIZATION: string
+  OPENAI_API_KEY: string
+}
 
-app.get("/", async (c) => {
-  try {
-    const { searchParams } = new URL(c.req.url);
-    const tweetUrl = searchParams.get("tweetUrl");
+const app = new Hono<{ Bindings: Bindings }>()
 
-    if (tweetUrl === null) {
-      return new Response("error: tweetUrl required.");
-    }
-    const tweetId = path.basename(url.parse(tweetUrl).pathname ?? "");
-
-    const text = await getTweetTextById(
-      c.env!.TWITTER_BEARER_TOKEN as string,
-      tweetId
-    );
-    console.log("text: " + text);
-
-    const openaiOrganization = c.env!.OPENAI_ORGANIZATION as string;
-    const openaiApiKey = c.env!.OPENAI_API_KEY as string;
-    const eventInfoJSON = await parseEventInfoFromText(
-      openaiOrganization,
-      openaiApiKey,
-      tweetUrl,
-      text
-    );
-    console.log("eventInfoJSON: " + eventInfoJSON);
-
-    const calendarUrl = await makeAddCalendarUrl(eventInfoJSON);
-    console.log("calendarUrl: " + calendarUrl);
-
-    return new Response(calendarUrl);
-  } catch (error) {
-    console.log(error);
-    return new Response("error");
+app.get('/', async (c) => {
+  const tweetUrl = c.req.query('tweetUrl')
+  if (!tweetUrl) {
+    return c.text('error: tweetUrl required.', 400)
   }
 
-  return c.json({
-    message: `Hello!`,
-  });
-});
+  const tweetId = new URL(tweetUrl).pathname.split('/').slice(-1)[0]
+  const text = await getTweetTextById(c.env.TWITTER_BEARER_TOKEN, tweetId)
+  console.log('text: ' + text)
 
-export default app;
+  const openaiOrganization = c.env.OPENAI_ORGANIZATION
+  const openaiApiKey = c.env.OPENAI_API_KEY
+  const eventInfoJSON = await parseEventInfoFromText(
+    openaiOrganization,
+    openaiApiKey,
+    tweetUrl,
+    text
+  )
+  console.log('eventInfoJSON: ' + eventInfoJSON)
+
+  const calendarUrl = makeAddCalendarUrl(eventInfoJSON)
+  console.log('calendarUrl: ' + calendarUrl)
+
+  return c.text(calendarUrl)
+})
+
+export default app
 
 // Tweetからテキストを取得する
 const getTweetTextById = async (twitterBearerToken: string, id: string) => {
-  const client = new Client(twitterBearerToken);
-  const tweet = await client.tweets.findTweetById(id);
-  return tweet.data?.text ?? "";
-};
+  const client = new Client(twitterBearerToken)
+  const tweet = await client.tweets.findTweetById(id)
+  return tweet.data?.text ?? ''
+}
 
 // chatGPT APIを使って、テキストからイベント情報を構造化してJSONで取得する
 const parseEventInfoFromText = async (
@@ -70,9 +59,9 @@ const parseEventInfoFromText = async (
     baseOptions: {
       adapter: fetchAdapter,
     },
-  });
-  const openai = new OpenAIApi(configuration);
-  const now = new Date();
+  })
+  const openai = new OpenAIApi(configuration)
+  const now = new Date()
   const chatgpt_spell = `
 	Twitterを元に、イベントをGoogleカレンダーに登録するアプリを作っています。
 	下記tweetを、下記フォーマットに構造化して、JSONを返してください。
@@ -92,29 +81,29 @@ const parseEventInfoFromText = async (
 	location: 場所（15文字程度）。
 	dates: 開始日時と終了日時。年が無い場合は今日の日付を元に、一番近い未来の日時にしてください。日時形式=20210401T093000Z/20210401T100000Z
 	sprop: URL。複数指定する場合は「sprop=〇〇&sprop=△△」
-	`;
+	`
 
-  const inputContent = chatgpt_spell;
-  console.log("content: " + inputContent);
+  const inputContent = chatgpt_spell
+  console.log('content: ' + inputContent)
   const result = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     temperature: 0.7,
     messages: [
       {
-        role: "user",
+        role: 'user',
         content: inputContent,
       },
     ],
-  });
-  const content = result.data.choices[0].message?.content;
-  return content ?? "";
-};
+  })
+  const content = result.data.choices[0].message?.content
+  return content ?? ''
+}
 
 // Googleカレンダーに追加するURLを作成する
 const makeAddCalendarUrl = (json: string) => {
-  const params = JSON.parse(json);
-  const url = "http://www.google.com/calendar/event";
-  const urlParams = querystring.stringify(params);
-  const calendarUrl = `${url}?${urlParams}`;
-  return calendarUrl;
-};
+  const params = JSON.parse(json)
+  const url = 'http://www.google.com/calendar/event'
+  const urlParams = new URLSearchParams(params).toString()
+  const calendarUrl = `${url}?${urlParams}`
+  return calendarUrl
+}
